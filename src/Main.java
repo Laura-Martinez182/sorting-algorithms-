2ca399680d6ee9e6775ee33817b193b3d5f3785d
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-02-24 21:41:55 -0500

    correction of methods and objects

--- src/Main.java
+++ src/Main.java
@@ -4,55 +4,85 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.text.DecimalFormat;
+
 
 
 public class Main {
+	
+	static BufferedWriter objWriter;
+	static BufferedReader objReader;
+	
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+    	
+    	objReader = new BufferedReader(new InputStreamReader(System.in));
+		objWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		BufferedReader objReader = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(objReader.readLine());
-		
-		for (int i=1;i<=n;i++) {
-			String[] cases = objReader.readLine().split(" ");
-			double[] ages = new double[cases.length];
-			
-			for (int j=0;j < cases.length;j++) {
-				ages[j] = Double.parseDouble(cases[j]);
-			 
-			}
-			
-			bubbleSort(ages);
-			 
-			  
-			 
-		}
-		
-	}
-	
-	
-	public static void bubbleSort(double[] ages) throws IOException {
-		int changes = 1;
-		for(int i= 1; i<ages.length && changes>0;i++) {
-			changes = 0;
-		    for(int j=0;j<ages.length-i;j++) {
-		    	if(ages[j]>ages[j+1]) {
-					double temp = ages[j];
-					ages[j] = ages[j+1];
-					ages[j+1] = temp;
-					changes++;
-		    	}
-			}
-			
-		}
-		
-		  BufferedWriter objWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-		  objWriter.write(changes+"");
-		  objWriter.write(Arrays.toString(ages));
-		
-	}
+        int n = Integer.parseInt(objReader.readLine());
+        System.out.println(" ");
+
+        for (int i=1;i<=n;i++) {
+            String[] cases = objReader.readLine().split(" ");
+            double[] ages = new double[cases.length];
+           
+
+            for (int j=0;j < cases.length;j++) {
+                ages[j] = Double.parseDouble(cases[j]);
+                
+            }
+            
+            
+            bubbleSort(ages);
+ }
+
+        
+        
+        objWriter.close();
+        objReader.close();
 }
+
+
+
+    public static void bubbleSort(double[] ages) throws IOException {
+
+        int changes = 0;
+        double pass = 0;
+        for(int i= 1; i<ages.length;i++) {
+            for(int j=0;j<ages.length-i;j++) {
+                if(ages[j]>ages[j+1]) {
+                    double temp = ages[j];
+                    ages[j] = ages[j+1];
+                    ages[j+1] = temp;
+                    changes++;
+                  
+                }
+                
+               
+            }
+            
+       
+            pass++;
+        }
+
+        
+        
+        DecimalFormat df = new DecimalFormat("#.00");
+        objWriter.write(df.format(changes/pass)+ "-");
+       
+        for(int c = 0; c < ages.length; c++) {
+            objWriter.write(String.valueOf(ages[c]) + " ");
+        }
+        objWriter.write("\n");
+        
+        objWriter.flush();
+
+        
+    }
+}
+   
+
+
 	
 
 	
